90690619d7b6d54988c055885c6f94e25622f855
Siffzaen09 <71373078+Siffzaen09@users.noreply.github.com>
2021-10-13 21:57:42 +0700

    Add files via upload

---
+++ Jurusan.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Jurusan {
+    private String kode;
+    private String nama;
+    private ArrayList<Mahasiswa> mhs = new ArrayList();
+
+    public Jurusan ( String kode, String nama ) {
+        this.kode = kode;
+        this.nama = nama;
+    }
+
+    public void addMahasiswa(Mahasiswa m){
+        this.mhs.add(m);
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public String getNama () {
+        return nama;
+    }
+
+    public ArrayList<Mahasiswa>  getMhs () {
+        return mhs;
+    }
+}
